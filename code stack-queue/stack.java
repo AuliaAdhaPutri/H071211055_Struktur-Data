0c0dcbf0760f389e4ac8699fd16c7801a90a9fc5
auliaadhaputri <auliaadhaputri17@gmail.com>
2022-12-17 19:37:25 +0800

    tugas stack dan queue

---
+++ code stack-queue/stack.java
@@ -0,0 +1,59 @@
+// Java code for stack implementation
+
+// import java.io.*;
+import java.util.*;
+
+class stack
+{
+	// Pushing element on the top of the stack
+	static void stack_push(Stack<Integer> stack)
+	{
+		for(int i = 0; i < 5; i++)
+		{
+			stack.push(i);
+		}
+	}
+	
+	// Popping element from the top of the stack
+	static void stack_pop(Stack<Integer> stack)
+	{
+		System.out.println("Pop Operation:");
+
+		for(int i = 0; i < 5; i++)
+		{
+			Integer y = (Integer) stack.pop();
+			System.out.println(y);
+		}
+	}
+
+	// Displaying element on the top of the stack
+	static void stack_peek(Stack<Integer> stack)
+	{
+		Integer element = (Integer) stack.peek();
+		System.out.println("Elemen yang berada di tumpukan paling atas: " + element);
+	}
+	
+	// Searching element in the stack
+	static void stack_search(Stack<Integer> stack, int element)
+	{
+		Integer pos = (Integer) stack.search(element);
+
+		if(pos == -1)
+			System.out.println("Elemen tidak ditemukan");
+		else
+			System.out.println("Elemen ditemukan diposisi: " + pos);
+	}
+
+
+	public static void main (String[] args)
+	{
+		Stack<Integer> stack = new Stack<Integer>();
+
+		stack_push(stack);
+		stack_pop(stack);
+		stack_push(stack);
+		stack_peek(stack);
+		stack_search(stack, 2);
+		stack_search(stack, 6);
+	}
+}
